94c0baa7c9d68e89421ba42e9b677cdeb3447199
Kevin Medeiros <108627831+medeirosnvk@users.noreply.github.com>
2025-09-30 15:54:40 -0300

    wip

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 const __filename = fileURLToPath(import.meta.url);
