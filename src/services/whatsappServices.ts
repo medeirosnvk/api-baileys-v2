44bd204f46e1e1197c578b7e3065ff6757d0ed51
Kevin Medeiros <108627831+medeirosnvk@users.noreply.github.com>
2025-10-01 15:15:45 -0300

    wip

--- src/services/whatsappServices.ts
+++ src/services/whatsappServices.ts
@@ -562,6 +562,9 @@ export class WhatsAppService {
       const __filename = fileURLToPath(import.meta.url);
       const __dirname = path.dirname(__filename);
 
+      console.log("__filename", __filename);
+      console.log("__dirname", __dirname);
+
       const qrPath = path.join(__dirname, "../../temp", `${connectionId}.png`);
 
       if (await fs.pathExists(qrPath)) {
